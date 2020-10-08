f8d31ca6dc190cfeb74c1cded6f18f3cc0fcb5e1
kirill <031kirill2a@mail.ru>
2020-10-08 17:29:57 +0300

    start

--- src/ru/vsu/cs/ivanov/Main.java
+++ src/ru/vsu/cs/ivanov/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         int a = enterNumber('a');
         int b = enterNumber('b');
-        System.out.print(sumOfNeededNumber(a, b));
+        System.out.print(sumOfNumberWithEvenOddDigits(a, b));
     }
 
     private static int enterNumber(char nameOfNumber) {
@@ -16,27 +16,27 @@ public class Main {
         return scan.nextInt();
     }
 
-    private static int sumOfNeededNumber(int a, int b) {
+    private static int sumOfNumberWithEvenOddDigits(int a, int b) {
         int sum = 0;
         for (int i = a; i < (b + 1); i++) {
-            sum += isNeededNumber(i);
+            sum += isNumberWithEvenOddDigits(i);
         }
         return sum;
     }
 
-    private static int isNeededNumber(int number) {
-        int countOfEvenNumber = 0;
-        int countOfOddNumber = 0;
+    private static int isNumberWithEvenOddDigits(int number) {
+        int countOfEvenDigits = 0;
+        int countOfOddDigits = 0;
         int testNumber = number;
         while (testNumber > 0) {
             if (testNumber % 2 == 0) {
-                countOfEvenNumber += 1;
+                countOfEvenDigits += 1;
             } else {
-                countOfOddNumber +=1;
+                countOfOddDigits +=1;
             }
             testNumber /= 10;
         }
-        if (countOfEvenNumber == countOfOddNumber) {
+        if (countOfEvenDigits == countOfOddDigits) {
             return number;
         } else {
             return 0;
